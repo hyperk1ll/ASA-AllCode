f7b01d8aedc61c5f35a5c34ba5e45f1c1106863d
Hilmy Tsany <hilmy.tsani@gmail.com>
2022-10-30 04:06:05 +0700

    11.Remed Ga Ya?

---
+++ Pertemuan 9/11.py
@@ -0,0 +1,18 @@
+# 11.Remed Ga Ya?
+
+def RemedGaYa(nilai):
+    if nilai >= 80 and nilai <=100:
+        return "Sangat Aman"
+    elif nilai >= 70 and nilai <= 79:
+        return "Aman"
+    elif (nilai >= 60 and nilai <= 69):
+        return "Lebih Baik Remed"
+
+    elif (nilai >= 0 and nilai <= 59):
+        return "Harus Remed"
+    else:
+        return "Input Invalid"
+    
+nilai = int(input())
+
+print(RemedGaYa(nilai))
