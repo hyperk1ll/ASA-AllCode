46e9643c2d6056ceea09747ecde76d661419a477
Hilmy Tsany <hilmy.tsani@gmail.com>
2022-11-01 19:20:04 +0700

    14.PRIMA adalah Segalanya

---
+++ Pertemuan 9/14.py
@@ -0,0 +1,20 @@
+# 14.PRIMA adalah Segalanya
+
+def Bilprima(x):
+    if x == 1:
+        return False
+    for i in range(2,x+1):
+        if x % i == 0:
+            if x == i:
+                return True
+            else:
+                return False
+
+def prima(x, y):
+    a = [["O" if Bilprima(i + j) == True else "X" for j in range(1, m + 1)] for i in range(1, n + 1)]
+
+    return a
+
+m, n = map(int, input().split())
+
+print(prima(m, n))
