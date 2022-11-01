8ad763efe6e8d9cce8acfb15ae2e0ef60c91c9d8
Hilmy Tsany <hilmy.tsani@gmail.com>
2022-11-01 19:24:12 +0700

    15.Hari Kebalikan

--- Pertemuan 9/15.py
+++ Pertemuan 9/15.py
@@ -1,1 +1,13 @@
 # 15.Hari Kebalikan
+
+def reverseString(str):
+    out = ""
+    
+    for i in range(len(str)):
+        out += str[len(str) -i - 1]
+    
+    return out
+
+str = input()
+
+print(reverseString(str))
